d9d448c76ad75f69c206584d501b96161f4d7eb7
Maxim Guryev <mg@smartvalley.io>
2017-11-21 11:11:23 +0000

    Merged in ILT-343 (pull request #15)
    edit root component
    
    * edit root component
    
    * fix rate colors
    
    * fix style
    
    * fix package
    
    * fix merge
    
    Approved-by: Valentin Livkin <itrk@rassvet-capital.ru>
    Approved-by: Konstantin Zhukov <kz@icolab.com>
    Approved-by: maksim.zakharov <mz@smartvalley.io>

--- SmartValley.FrontEnd/src/app/components/root/root.component.ts
+++ SmartValley.FrontEnd/src/app/components/root/root.component.ts
@@ -3,6 +3,7 @@ import {AuthenticationService} from '../../services/authentication-service';
 import {Web3Service} from '../../services/web3-service';
 import {Router} from '@angular/router';
 import {Paths} from '../../paths';
+import {Project} from '../../services/project';
 import {NotificationsService} from 'angular2-notifications';
 
 @Component({
@@ -13,12 +14,14 @@ import {NotificationsService} from 'angular2-notifications';
 export class RootComponent implements OnInit {
 
   public userInfo: User;
+  public projects: Array<Project>;
 
   constructor(private web3Service: Web3Service,
               private authenticationService: AuthenticationService,
               private notificationsService: NotificationsService,
               private router: Router) {
     // this.authenticationService.userInfoChanged.subscribe(async () => await this.updateUserInfo());
+    this.initTestData();
   }
 
   async ngOnInit() {
@@ -38,5 +41,43 @@ export class RootComponent implements OnInit {
     if (isOk) {
       await this.router.navigate([Paths.Application]);
     }
+  }
+
+  colorOfProjectRate(rate: string) {
+    const r = parseInt(rate, 10)
+    if (r > 80) { return 'high_rate'; }
+    if (r > 45) { return 'medium_rate'; }
+    return 'low_rate';
+  }
+
+  // тестовые данные
+  initTestData() {
+    this.projects = [];
+    this.projects.push(<Project>{
+      projectName: 'Rega Risk Sharing',
+      projectArea: 'Crowdsurance',
+      projectCountry: 'Russia',
+      scoringRating: '99',
+      projectDescription: 'In literary theory, a text is any object that can be "read", whether this object is a work of literature, a street sign, an arrangement of buildings on a city block, or styles of clothing.',
+      projectImgUrl: 'https://png.icons8.com/?id=50284&size=280'
+    });
+
+    this.projects.push(<Project>{
+      projectName: 'BitClave Active Search Ecosystem',
+      projectArea: 'Rotetechnology',
+      projectCountry: 'Russia',
+      scoringRating: '65',
+      projectDescription: 'In literary theory, a text is any object that can be "read", whether this object is a work of literature, a street sign, an arrangement of buildings on a city block, or styles of clothing.',
+      projectImgUrl: 'https://png.icons8.com/?id=50284&size=280'
+    });
+
+    this.projects.push(<Project>{
+      projectName: 'B2Broker',
+      projectArea: 'Brokering',
+      projectCountry: 'Russia',
+      scoringRating: '8',
+      projectDescription: 'In literary theory, a text is any object that can be "read", whether this object is a work of literature, a street sign, an arrangement of buildings on a city block, or styles of clothing.',
+      projectImgUrl: 'https://png.icons8.com/?id=50284&size=280'
+    });
   }
 }
---
+++ SmartValley.FrontEnd/src/app/services/project.ts
@@ -0,0 +1,9 @@
+export interface Project {
+
+  projectName: string;
+  projectImgUrl: string;
+  projectCountry: string;
+  projectArea: string;
+  projectDescription: string;
+  scoringRating: string;
+}
