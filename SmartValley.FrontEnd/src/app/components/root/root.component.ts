57390461bae5c38aac827af86580b2162da42012
mguriev <mg@smartvalley.io>
2017-11-21 10:58:17 +0300

    edit root component

--- SmartValley.FrontEnd/src/app/components/root/root.component.ts
+++ SmartValley.FrontEnd/src/app/components/root/root.component.ts
@@ -5,6 +5,7 @@ import {NotificationService} from '../../services/notification-service';
 import {UserInfo} from '../../services/user-info';
 import {Router} from '@angular/router';
 import {Paths} from '../../paths';
+import {Project} from '../../services/project';
 
 @Component({
   selector: 'app-root',
@@ -14,12 +15,14 @@ import {Paths} from '../../paths';
 export class RootComponent implements OnInit {
 
   public userInfo: UserInfo;
+  public projects: Array<Project>;
 
   constructor(private web3Service: Web3Service,
               private authenticationService: AuthenticationService,
               private notificationService: NotificationService,
               private router: Router) {
     this.authenticationService.userInfoChanged.subscribe(async () => await this.updateUserInfo());
+    this.initTestData();
   }
 
   async ngOnInit() {
@@ -51,5 +54,43 @@ export class RootComponent implements OnInit {
       this.userInfo = await this.authenticationService.getUserInfo();
       await this.router.navigate([Paths.Application]);
     }
+  }
+
+  colorOfProjectRate(rate: string) {
+    const r = parseInt(rate, 10)
+    if (r > 80) { return 'high_rate'; }
+    if (r > 45) { return 'medium_rate'; }
+    return 'low_rate';
+  }
+
+  // тестовые данные
+  initTestData() {
+    this.projects = [];
+    this.projects.push(<Project>{
+      projectName: 'Rega Risk Sharing',
+      projectArea: 'Crowdsurance',
+      projectCountry: 'Russia',
+      scoringRating: '99',
+      projectDescription: 'In literary theory, a text is any object that can be "read", whether this object is a work of literature, a street sign, an arrangement of buildings on a city block, or styles of clothing.',
+      projectImgUrl: 'https://png.icons8.com/?id=50284&size=280'
+    });
+
+    this.projects.push(<Project>{
+      projectName: 'BitClave Active Search Ecosystem',
+      projectArea: 'Rotetechnology',
+      projectCountry: 'Russia',
+      scoringRating: '65',
+      projectDescription: 'In literary theory, a text is any object that can be "read", whether this object is a work of literature, a street sign, an arrangement of buildings on a city block, or styles of clothing.',
+      projectImgUrl: 'https://png.icons8.com/?id=50284&size=280'
+    });
+
+    this.projects.push(<Project>{
+      projectName: 'B2Broker',
+      projectArea: 'Brokering',
+      projectCountry: 'Russia',
+      scoringRating: '8',
+      projectDescription: 'In literary theory, a text is any object that can be "read", whether this object is a work of literature, a street sign, an arrangement of buildings on a city block, or styles of clothing.',
+      projectImgUrl: 'https://png.icons8.com/?id=50284&size=280'
+    });
   }
 }
