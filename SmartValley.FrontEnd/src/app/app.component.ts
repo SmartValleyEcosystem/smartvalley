5e901bdd7aa9bf60ae315d035fc2a63d72cba3aa
Zakharov Maksim <Max89701@gmail.com>
2017-12-05 15:14:01 +0300

    load question in appcomponent

--- SmartValley.FrontEnd/src/app/app.component.ts
+++ SmartValley.FrontEnd/src/app/app.component.ts
@@ -1,16 +1,22 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Options} from 'angular2-notifications';
+import {QuestionService} from './services/question-service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   notifyOptions: Options = {
     position: ['top', 'left'],
     timeOut: 3000,
     animate: 'fromLeft',
     showProgressBar: false
   };
+  constructor(private questionService: QuestionService) {
+  }
+  async ngOnInit() {
+    await this.questionService.initializeQuestionsCollection();
+  }
 }
--- SmartValley.FrontEnd/src/app/services/question-service.ts
+++ SmartValley.FrontEnd/src/app/services/question-service.ts
@@ -1,13 +1,9 @@
 import {Injectable} from '@angular/core';
-import {Question} from './question';
 import {ExpertiseArea} from '../api/scoring/expertise-area.enum';
 import {HttpClient} from '@angular/common/http';
 import {BaseApiClient} from '../api/base-api-client';
 import {GetQuestionsResponse} from '../services/get-questions-response';
-import {isObject} from 'rxjs/util/isObject';
 import {QuestionResponse} from '../api/estimates/question-response';
-import {Estimate} from './estimate';
-
 
 @Injectable()
 export class QuestionService extends BaseApiClient {
@@ -17,31 +13,21 @@ export class QuestionService extends BaseApiClient {
     super();
   }
 
-  public async getByCategory(category: ExpertiseArea): Promise<Array<QuestionResponse>> {
-    await this.checkAndFill();
+  public getByCategory(category: ExpertiseArea): Array<QuestionResponse> {
     return this.questions[category];
   }
 
-  private async checkAndFill() {
-    if (isObject(this.questions)) {
-      await this.initializeQuestionsCollection();
-    }
-  }
-
-
-  public async getMaxScoreForCategory(category: ExpertiseArea): Promise<number> {
-    await this.checkAndFill();
+  public getMaxScoreForCategory(category: ExpertiseArea): number {
     const categoryQuestions = this.questions[category];
     return categoryQuestions.map(q => q.maxScore).reduce((previous, current) => previous + current);
   }
 
-  public async getMinScoreForCategory(category: ExpertiseArea): Promise<number> {
-    await this.checkAndFill();
+  public getMinScoreForCategory(category: ExpertiseArea): number {
     const categoryQuestions = this.questions[category];
     return categoryQuestions.map(q => q.minScore).reduce((previous, current) => previous + current);
   }
 
-  private async initializeQuestionsCollection() {
+  public async initializeQuestionsCollection() {
     const allQuestions = await this.http.get<GetQuestionsResponse>(this.baseApiUrl + '/estimates/questions').toPromise();
     for (const item in ExpertiseArea) {
       if (Number(item)) {
