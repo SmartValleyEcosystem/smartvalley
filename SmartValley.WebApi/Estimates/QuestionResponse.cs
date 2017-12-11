0911a5004a0031731619c96a5f843b73477e1f1c
Fedorov Evgeniy <ef@icolab.com>
2017-12-11 18:05:12 +0300

    removed question's name and desc from DB, typo fixes

--- SmartValley.WebApi/Estimates/QuestionResponse.cs
+++ SmartValley.WebApi/Estimates/QuestionResponse.cs
@@ -6,22 +6,16 @@ namespace SmartValley.WebApi.Estimates
     {
         public long Id { get; set; }
 
-        public string Name { get; set; }
-
-        public string Description { get; set; }
-
         public ExpertiseArea ExpertiseArea { get; set; }
 
         public int MinScore { get; set; }
 
         public int MaxScore { get; set; }
-
+        
         public static QuestionResponse From(Question question)
         {
             return new QuestionResponse
                    {
-                       Description = question.Description,
-                       Name = question.Name,
                        Id = question.Id,
                        ExpertiseArea = question.ExpertiseArea.FromDomain(),
                        MaxScore = question.MaxScore,
