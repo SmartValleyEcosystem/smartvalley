882d439823918bbb0ae93dbd90229997c096635a
Zakharov Maksim <Max89701@gmail.com>
2017-12-21 16:31:35 +0300

    change inject date

--- SmartValley.FrontEnd/src/app/components/common/receive-svt-modal/receive-svt-modal.component.ts
+++ SmartValley.FrontEnd/src/app/components/common/receive-svt-modal/receive-svt-modal.component.ts
@@ -8,7 +8,7 @@ import {MAT_DIALOG_DATA} from '@angular/material';
 })
 export class ReceiveSvtModalComponent {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: {date: string}) {
   }
 
 }
