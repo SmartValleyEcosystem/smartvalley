0a180cf4fb54f5ebb5d0b2faabc246fef77e7c7e
maksim.zakharov <mz@smartvalley.io>
2018-02-22 12:18:38 +0000

    Merged in ILT-615 (pull request #179)
    ILT-615

--- SmartValley.FrontEnd/src/app/services/authentication/authentication-service.ts
+++ SmartValley.FrontEnd/src/app/services/authentication/authentication-service.ts
@@ -151,6 +151,7 @@ export class AuthenticationService {
         signedText: messageToSign,
         signature: signature
       });
+      await this.userContext.saveSignatureForAccount(account, signature);
       return true;
     } catch (e) {
       if (e.error.errorCode === ErrorCode.EmailSendingFailed) {
