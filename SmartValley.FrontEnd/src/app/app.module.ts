d1cd739277c52091a53ac0a1952582cec4eb6953
Viktor Kabanov <kabanov-viktor92@mail.ru>
2018-03-07 08:15:14 +0000

    Merged in ILT-658 (pull request #201)
    ILT-658
    
    * ILT-658 add Work place and tabs with route (frontend)
    
    * ILT-658 add tabs expert work place
    
    * ILT-658 fix guard
    
    * ILT-658 add fixes for review
    
    Approved-by: Konstantin Zhukov <kz@icolab.com>
    Approved-by: maksim.zakharov <mz@smartvalley.io>

--- SmartValley.FrontEnd/src/app/app.module.ts
+++ SmartValley.FrontEnd/src/app/app.module.ts
@@ -113,6 +113,8 @@ import {ExpertStatusGuard} from './services/guards/expert-status.guard';
 import {AdminScoringProjectsComponent} from './components/admin-panel/admin-scoring-projects/admin-scoring-projects.component';
 import {SetExpertsModalComponent} from './components/common/set-experts-modal/set-experts-modal.component';
 import {ChangeEmailModalComponent} from './components/common/change-email-modal/change-email-modal.component';
+import {ExpertWorkPlaceComponent} from './components/expert/expert-work-place/expert-work-place.component';
+import {OffersApiClient} from './api/expert/offers-api-client';
 
 @NgModule({
   declarations: [
@@ -165,7 +167,8 @@ import {ChangeEmailModalComponent} from './components/common/change-email-modal/
     AdminExpertApplicationComponent,
     AdminScoringProjectsComponent,
     SetExpertsModalComponent,
-    ChangeEmailModalComponent
+    ChangeEmailModalComponent,
+    ExpertWorkPlaceComponent
   ],
   entryComponents: [
     TransactionAwaitingModalComponent,
@@ -271,7 +274,8 @@ import {ChangeEmailModalComponent} from './components/common/change-email-modal/
     ExpertStatusGuard,
     RegisterExpertGuard,
     AreaService,
-    EnumHelper
+    EnumHelper,
+    OffersApiClient
   ],
   bootstrap: [AppComponent]
 })
