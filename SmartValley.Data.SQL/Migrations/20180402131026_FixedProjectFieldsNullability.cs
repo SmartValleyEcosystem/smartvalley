29ef934451798a1a2acdb4f7756a4329a277609f
Konstantin Zhukov <kz@icolab.com>
2018-04-02 13:39:19 +0000

    Merged in ILT-893 (pull request #309)
    ILT-893
    
    Approved-by: maksim.zakharov <mz@smartvalley.io>
    Approved-by: Valentin Livkin <vl@icolab.com>

---
+++ SmartValley.Data.SQL/Migrations/20180402131026_FixedProjectFieldsNullability.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SmartValley.Data.SQL.Migrations
+{
+    public partial class FixedProjectFieldsNullability : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "IcoDate",
+                table: "Projects",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Website",
+                table: "Projects",
+                maxLength: 200,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
