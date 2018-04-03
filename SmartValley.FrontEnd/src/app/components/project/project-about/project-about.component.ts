7a4a553343ecf161c1bffc6a9e4e0eb77ac2e01d
mguriev <mg@smartvalley.io>
2018-04-03 17:06:30 +0300

    fix social links in project about

--- SmartValley.FrontEnd/src/app/components/project/project-about/project-about.component.ts
+++ SmartValley.FrontEnd/src/app/components/project/project-about/project-about.component.ts
@@ -23,13 +23,13 @@ export class ProjectAboutComponent implements OnInit {
   }
 
   private checkSocials(): boolean {
-    return !isNullOrUndefined(this.projectInfo.bitcoinTalk) &&
-      !isNullOrUndefined(this.projectInfo.facebook) &&
-      !isNullOrUndefined(this.projectInfo.github) &&
-      !isNullOrUndefined(this.projectInfo.linkedin) &&
-      !isNullOrUndefined(this.projectInfo.medium) &&
-      !isNullOrUndefined(this.projectInfo.reddit) &&
-      !isNullOrUndefined(this.projectInfo.telegram) &&
+    return !isNullOrUndefined(this.projectInfo.bitcoinTalk) ||
+      !isNullOrUndefined(this.projectInfo.facebook) ||
+      !isNullOrUndefined(this.projectInfo.github) ||
+      !isNullOrUndefined(this.projectInfo.linkedin) ||
+      !isNullOrUndefined(this.projectInfo.medium) ||
+      !isNullOrUndefined(this.projectInfo.reddit) ||
+      !isNullOrUndefined(this.projectInfo.telegram) ||
       !isNullOrUndefined(this.projectInfo.twitter)
       ;
   }
