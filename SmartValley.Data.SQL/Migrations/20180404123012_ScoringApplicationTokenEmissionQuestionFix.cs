9d375c3e1315b3c32f1784c388c0d405715aee50
Konstantin Zhukov <kz@icolab.com>
2018-04-04 12:38:29 +0000

    Merged in ILT-936 (pull request #348)
    ILT-936

---
+++ SmartValley.Data.SQL/Migrations/20180404123012_ScoringApplicationTokenEmissionQuestionFix.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SmartValley.Data.SQL.Migrations
+{
+    public partial class ScoringApplicationTokenEmissionQuestionFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"UPDATE [dbo].[ScoringApplicationQuestions]
+                                   SET [ParentTriggerValue] = N'0'
+                                   WHERE [Key] = N'TokenEmissionRestrictionDescriptionQuestion'");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"UPDATE [dbo].[ScoringApplicationQuestions]
+                                   SET [ParentTriggerValue] = N'1'
+                                   WHERE [Key] = N'TokenEmissionRestrictionDescriptionQuestion'");
+        }
+    }
+}
