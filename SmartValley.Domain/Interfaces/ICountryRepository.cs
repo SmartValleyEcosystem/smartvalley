9c77a2bb945d9442f46558db43b43b2948410f1b
Zakharov Maksim <Max89701@gmail.com>
2018-05-17 10:42:54 +0200

    fix
    fix

--- SmartValley.Domain/Interfaces/ICountryRepository.cs
+++ SmartValley.Domain/Interfaces/ICountryRepository.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using SmartValley.Domain.Entities;
 
 namespace SmartValley.Domain.Interfaces
@@ -7,9 +6,5 @@ namespace SmartValley.Domain.Interfaces
     public interface ICountryRepository
     {
         Task<Country> GetByCodeAsync(string code);
-
-        Task<Country> GetByIdAsync(long id);
-
-        Task<IReadOnlyCollection<Country>> GetAllAsync();
     }
 }
