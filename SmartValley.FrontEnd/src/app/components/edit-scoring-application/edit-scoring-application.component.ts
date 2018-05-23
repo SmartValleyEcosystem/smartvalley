40030050bb2d637dd752e69aba86a36f1b98274e
mguriev <mg@smartvalley.io>
2018-05-23 14:51:21 +0300

    fix build

--- SmartValley.FrontEnd/src/app/components/edit-scoring-application/edit-scoring-application.component.ts
+++ SmartValley.FrontEnd/src/app/components/edit-scoring-application/edit-scoring-application.component.ts
@@ -81,37 +81,6 @@ export class EditScoringApplicationComponent implements OnInit, OnDestroy {
           }
         };
 
-  public editorFormats = [
-        'bold',
-        'underline',
-        'strike',
-        'header',
-        'italic',
-        'list',
-        'indent',
-        'color',
-        'align',
-        'background'
-      ];
-  public editorOptions = {
-          toolbar: {
-              container:
-                  [
-                      ['bold', 'italic', 'underline', 'strike'],
-                      [{ 'header': 1 }, { 'header': 2 }],
-                      [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-                      [{ 'indent': '-1' }, { 'indent': '+1' }],
-                      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-                      [{ 'color': [] }, { 'background': [] }],
-                      [{ 'align': [] }],
-
-                  ]
-              },
-          clipboard: {
-              matchVisual: false
-          }
-        };
-
   @ViewChild('socialsContainer') private socialsContainer: ElementRef;
   @ViewChild('membersContainer') private membersContainer: ElementRef;
   @ViewChildren('required') public requiredFields: QueryList<any>;
