0d2d78d47e1ac24ab5adc0a1bba81ba8efaa62e0
Zakharov Maksim <Max89701@gmail.com>
2018-05-30 17:33:17 +0200

    frontend logging has been added

---
+++ SmartValley.WebApi/Logging/LoggingController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SmartValley.WebApi.Logging.Requests;
+
+namespace SmartValley.WebApi.Logging
+{
+    [Route("api/logging")]
+    public class LoggingController : Controller
+    {
+        private readonly ILogger _logger;
+
+        public LoggingController(ILogger<LoggingController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("error")]
+        public IActionResult Post([FromBody] LogErrorRequest request)
+        {
+            _logger.LogError($"User: '{User.Identity.Name}', {request}");
+            return NoContent();
+        }
+    }
+}
