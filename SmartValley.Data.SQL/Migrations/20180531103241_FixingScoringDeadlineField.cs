14c00651c894318b8454fd1295a66f88021899f5
Sergey Popov <sp@icolab.com>
2018-05-31 13:39:12 +0300

    Fixing ScoringDealine field.

---
+++ SmartValley.Data.SQL/Migrations/20180531103241_FixingScoringDeadlineField.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SmartValley.Data.SQL.Migrations
+{
+    public partial class FixingScoringDeadlineField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"update Scorings
+                                   set ScoringDeadline = DATEADD(d, 4, CreationDate)");
+
+            migrationBuilder.Sql(@"alter table Scorings
+                                   alter column ScoringDeadline datetimeoffset(7) not null");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
