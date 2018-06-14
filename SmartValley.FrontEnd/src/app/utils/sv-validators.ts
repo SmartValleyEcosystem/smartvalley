c8b4d51e1c0cc600c0163cf843562126386a14a4
Zakharov Maksim <Max89701@gmail.com>
2018-06-14 14:47:14 +0200

    fix
    fix
    
    
    fix
    
    
    fix
    
    
    fix

---
+++ SmartValley.FrontEnd/src/app/utils/sv-validators.ts
@@ -0,0 +1,13 @@
+import {AbstractControl, ValidationErrors, ValidatorFn, Validators} from '@angular/forms';
+import {isNullOrUndefined} from 'util';
+
+export class SVValidators extends Validators {
+  static checkFutureDate(control: AbstractControl) {
+    if (isNullOrUndefined(control.value)) {
+      return null;
+    }
+    const currentDate = new Date(Date.now());
+    const valueDate = (control.value as Date);
+    return valueDate > currentDate ? null : {dateIsNotFuture: true};
+  }
+}
