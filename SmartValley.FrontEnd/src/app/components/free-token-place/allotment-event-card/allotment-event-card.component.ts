5a8d15bfca3c200cf7dd60c270cf76003f23e57f
Kabanov Viktor <kabanov-viktor92@mail.ru>
2018-06-19 13:05:57 +0300

    SV-1313 add fixes for review

--- SmartValley.FrontEnd/src/app/components/free-token-place/allotment-event-card/allotment-event-card.component.ts
+++ SmartValley.FrontEnd/src/app/components/free-token-place/allotment-event-card/allotment-event-card.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {Component, OnInit, Input, OnDestroy, EventEmitter, Output} from '@angular/core';
 import {Paths} from '../../../paths';
 import {Router} from '@angular/router';
 import {BlockiesService} from '../../../services/blockies-service';
@@ -9,16 +9,24 @@ import {AllotmentEventCard} from '../allotment-event-card';
     templateUrl: './allotment-event-card.component.html',
     styleUrls: ['./allotment-event-card.component.scss']
 })
-export class AllotmentEventCardComponent implements OnInit {
+export class AllotmentEventCardComponent implements OnInit, OnDestroy {
 
     constructor(private router: Router,
                 private blockiesService: BlockiesService) {
     }
 
+    private timer: NodeJS.Timer;
+
     @Input() public event: AllotmentEventCard;
     @Input() public finished = false;
+    @Output() finishEvent: EventEmitter<number> = new EventEmitter<number>();
 
     ngOnInit() {
+        this.timer = <NodeJS.Timer>setInterval(async () => await this.getAllotmentEventTimeLeft(), 1000);
+    }
+
+    ngOnDestroy(): void {
+        clearInterval(this.timer);
     }
 
     public getProjectLink(id) {
@@ -29,5 +37,34 @@ export class AllotmentEventCardComponent implements OnInit {
 
     public imageUrl(): string {
         return this.event.project.imageUrl || this.blockiesService.getImageForAddress(this.event.project.authorAddress);
+    }
+
+    public pad(n) {
+        return (n < 10 ? '0' : '') + n;
+    }
+
+    public getAllotmentEventTimeLeft() {
+        const eventDate = new Date(this.event.finishDate);
+        const currentDate = new Date().getTime();
+
+        let secondsLeft = (eventDate - currentDate) / 1000;
+
+        const days = this.pad(parseInt(secondsLeft / 86400));
+        secondsLeft = secondsLeft % 86400;
+
+        const hours = this.pad(parseInt(secondsLeft / 3600));
+        secondsLeft = secondsLeft % 3600;
+
+        const minutes = this.pad(parseInt(secondsLeft / 60));
+        const seconds = this.pad(parseInt(secondsLeft % 60));
+
+        if (new Date() < eventDate) {
+            this.event.timer.days = days;
+            this.event.timer.hours = hours;
+            this.event.timer.minutes = minutes;
+            this.event.timer.seconds = seconds;
+        } else {
+            this.finishEvent.emit(this.event.id);
+        }
     }
 }
--- SmartValley.FrontEnd/src/app/components/free-token-place/free-token-place.component.ts
+++ SmartValley.FrontEnd/src/app/components/free-token-place/free-token-place.component.ts
@@ -14,7 +14,7 @@ import {ProjectResponse} from '../../api/project/project-response';
     templateUrl: './free-token-place.component.html',
     styleUrls: ['./free-token-place.component.scss']
 })
-export class FreeTokenPlaceComponent implements OnInit, OnDestroy {
+export class FreeTokenPlaceComponent implements OnInit {
 
     public allotmentEvents: AllotmentEventCard[];
     public activeEvents: AllotmentEventCard[] = [];
@@ -23,7 +23,6 @@ export class FreeTokenPlaceComponent implements OnInit, OnDestroy {
     public offset = 0;
     public pageSize = 10;
     public projects: ProjectResponse[] = [];
-    private timer: NodeJS.Timer;
 
     constructor(private allotmentEventsApiClient: AllotmentEventsApiClient,
                 private projectApiClient: ProjectApiClient) {
@@ -31,11 +30,6 @@ export class FreeTokenPlaceComponent implements OnInit, OnDestroy {
 
     async ngOnInit() {
         await this.loadAllotmentEventsAsync();
-        this.timer = <NodeJS.Timer>setInterval(async () => await this.getAllotmentEventTimeLeft(), 1000);
-    }
-
-    ngOnDestroy(): void {
-        clearInterval(this.timer);
     }
 
     private async loadAllotmentEventsAsync(): Promise<void> {
@@ -56,6 +50,7 @@ export class FreeTokenPlaceComponent implements OnInit, OnDestroy {
         const projectResponse = await this.projectApiClient.getAsync(<ProjectQuery>{
             offset: 0,
             count: projectIds.length,
+            onlyScored: false,
             orderBy: ProjectsOrderBy.CreationDate,
             direction: SortDirection.Descending,
             projectIds: projectIds
@@ -80,35 +75,10 @@ export class FreeTokenPlaceComponent implements OnInit, OnDestroy {
         });
     }
 
-    public pad(n) {
-        return (n < 10 ? '0' : '') + n;
-    }
-
-    public getAllotmentEventTimeLeft() {
-        for (let i = 0; this.activeEvents.length > i; i++) {
-            const eventDate = new Date(this.activeEvents[i].finishDate);
-            const currentDate = new Date().getTime();
-
-            let secondsLeft = (eventDate - currentDate) / 1000;
-
-            const days = this.pad(parseInt(secondsLeft / 86400));
-            secondsLeft = secondsLeft % 86400;
-
-            const hours = this.pad(parseInt(secondsLeft / 3600));
-            secondsLeft = secondsLeft % 3600;
-
-            const minutes = this.pad(parseInt(secondsLeft / 60));
-            const seconds = this.pad(parseInt(secondsLeft % 60));
-
-            if (new Date() < eventDate) {
-                this.activeEvents[i].timer.days = days;
-                this.activeEvents[i].timer.hours = hours;
-                this.activeEvents[i].timer.minutes = minutes;
-                this.activeEvents[i].timer.seconds = seconds;
-            }else {
-                this.finishedEvents.push(this.activeEvents[i]);
-                this.activeEvents.splice(i, 1);
-            }
-        }
+    public finishEvent(id: number) {
+        this.finishedEvents.push(   this.activeEvents.find((a) => a.id === id )   );
+        this.activeEvents = this.activeEvents.filter( (a, i) => {
+          return a.id !== id;
+        };
     }
 }
