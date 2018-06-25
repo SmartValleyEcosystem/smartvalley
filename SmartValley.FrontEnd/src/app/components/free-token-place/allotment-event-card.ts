5c8739674f2877ee185b43c1167de841d6d45c5b
Kabanov Viktor <kabanov-viktor92@mail.ru>
2018-06-25 11:45:37 +0200

    SV-1379 hide toolbar for unlogined users, get my bet and total bet
    SV-1379 add fixes for revirew
    
    SV-1379 add fixes for revirew
    
    SV-1379 fix for review

--- SmartValley.FrontEnd/src/app/components/free-token-place/allotment-event-card.ts
+++ SmartValley.FrontEnd/src/app/components/free-token-place/allotment-event-card.ts
@@ -4,17 +4,17 @@ import {ProjectResponse} from '../../api/project/project-response';
 import {AllotmentEventParticipantResponse} from '../../api/allotment-events/responses/allotment-event-participant-response';
 
 export interface AllotmentEventCard {
-  id?: number;
-  name: string;
-  status: AllotmentEventStatus;
-  tokenContractAddress: string;
-  eventContractAddress?: string;
-  startDate?: Date;
-  tokenDecimals: number;
-  tokenTicker: string;
-  projectId?: number;
-  finishDate?: Date;
-  participants: Array<AllotmentEventParticipantResponse>;
-  timer?: AllotmentEventTimer;
-  project?: ProjectResponse;
+    id: number;
+    name: string;
+    status: AllotmentEventStatus;
+    tokenContractAddress: string;
+    eventContractAddress?: string;
+    startDate?: Date;
+    tokenDecimals: number;
+    tokenTicker: string;
+    projectId?: number;
+    finishDate?: Date;
+    timer?: AllotmentEventTimer;
+    project?: ProjectResponse;
+    participants: Array<AllotmentEventParticipantResponse>;
 }
