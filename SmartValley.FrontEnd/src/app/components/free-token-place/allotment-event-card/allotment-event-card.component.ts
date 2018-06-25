a8b6dd3393377850c11e1bd5ee2496e8dff224a5
Kabanov Viktor <kabanov-viktor92@mail.ru>
2018-06-25 17:03:14 +0200

    SV-1378 show numbers with decimals
    SV-1378 show numbers with decimals
    
    SV-1378 add fixes for review

--- SmartValley.FrontEnd/src/app/components/free-token-place/allotment-event-card/allotment-event-card.component.ts
+++ SmartValley.FrontEnd/src/app/components/free-token-place/allotment-event-card/allotment-event-card.component.ts
@@ -6,6 +6,7 @@ import {DialogService} from '../../../services/dialog-service';
 import {Balance} from '../../../services/balance/balance';
 import {AuthenticationService} from '../../../services/authentication/authentication-service';
 import {AllotmentEventParticipateDialogData} from '../../common/allotment-event-participate-modal/allotment-event-participate-dialog-data';
+import {User} from '../../../services/authentication/user';
 import {UserContext} from '../../../services/authentication/user-context';
 import {isNullOrUndefined} from 'util';
 import {Router} from '@angular/router';
@@ -13,118 +14,125 @@ import {Paths} from '../../../paths';
 import {AllotmentEventService} from '../../../services/allotment-event/allotment-event.service';
 
 @Component({
-    selector: 'app-allotment-event-card',
-    templateUrl: './allotment-event-card.component.html',
-    styleUrls: ['./allotment-event-card.component.scss']
+  selector: 'app-allotment-event-card',
+  templateUrl: './allotment-event-card.component.html',
+  styleUrls: ['./allotment-event-card.component.scss']
 })
 export class AllotmentEventCardComponent implements OnInit, OnDestroy {
 
-    constructor(private router: Router,
-                private blockiesService: BlockiesService,
-                private allotmentEventService: AllotmentEventService,
-                private dialogService: DialogService,
-                private userContext: UserContext,
-                private authenticationService: AuthenticationService,
-                private erc223ContractClient: Erc223ContractClient) {
+  constructor(private router: Router,
+              private blockiesService: BlockiesService,
+              private allotmentEventService: AllotmentEventService,
+              private dialogService: DialogService,
+              private userContext: UserContext,
+              private authenticationService: AuthenticationService,
+              private erc223ContractClient: Erc223ContractClient) {
+  }
+
+  private timer: NodeJS.Timer;
+  public tokenBalance: number;
+  public user: User;
+  public myBid = 0;
+  public totalBid: number;
+  public canRecieveTokens: boolean;
+
+  @Input() public event: AllotmentEventCard;
+  @Input() public finished = false;
+  @Input() public balance: Balance;
+
+  @Output() finishEvent: EventEmitter<number> = new EventEmitter<number>();
+
+  async ngOnInit() {
+    this.user = this.userContext.getCurrentUser();
+    this.timer = <NodeJS.Timer>setInterval(async () => await this.getAllotmentEventTimeLeft(), 1000);
+    if (this.user) {
+        const myBid = this.event.participants.find((a) => a.userId === this.user.id);
+        this.myBid = myBid ? myBid.bid : 0;
     }
 
-    private timer: NodeJS.Timer;
-    public tokenBalance: number;
-    public myBet: number;
-    public totalBet: number;
-    public canRecieveTokens: boolean;
+    this.totalBid = this.event.participants.reduce((sum, c) => sum + c.bid, 0);
+    this.event.timer = {
+        days: '00',
+        hours: '00',
+        minutes: '00',
+        seconds: '00'
+    };
 
-    @Input() public event: AllotmentEventCard;
-    @Input() public finished = false;
-    @Input() public balance: Balance;
+    this.tokenBalance = await this.erc223ContractClient.getTokenBalanceAsync(this.event.tokenContractAddress, this.event.eventContractAddress);
 
-    @Output() finishEvent: EventEmitter<number> = new EventEmitter<number>();
+    this.canRecieveTokens = !isNullOrUndefined(this.event.participants) && this.finished
+        || !this.event.participants.some(i => i.userId === this.userContext.getCurrentUser().id && i.isCollected) && this.finished;
+  }
 
-    async ngOnInit() {
-        this.timer = <NodeJS.Timer>setInterval(async () => await this.getAllotmentEventTimeLeft(), 1000);
-        this.myBet = 60000;
-        this.totalBet = 174599;
-        this.event.timer = {
-            days: '00',
-            hours: '00',
-            minutes: '00',
-            seconds: '00'
-        };
+  ngOnDestroy(): void {
+    clearInterval(this.timer);
+  }
 
-        this.tokenBalance = await this.erc223ContractClient.getTokenBalanceAsync(this.event.tokenContractAddress, this.event.eventContractAddress);
+  public getPercentShare() {
+    return (this.myBid * 100) / this.totalBid;
+  }
 
-        this.canRecieveTokens = !isNullOrUndefined(this.event.participants)
-            || !this.event.participants.some(i => i.userId === this.userContext.getCurrentUser().id && i.isCollected) && this.finished;
+  public async showParticipateDialogAsync() {
+    if (await this.authenticationService.authenticateAsync()) {
+      const participateResult = await this.dialogService.showParticipateDialog(<AllotmentEventParticipateDialogData>{
+        balance: this.balance,
+        totalBet: this.totalBid,
+        myBet: this.myBid,
+        tokenBalance: this.tokenBalance,
+        decimals: this.event.tokenDecimals
+      });
+      if (participateResult) {
+        await this.allotmentEventService.participateAsync(this.event.id, participateResult);
+      }
     }
+  }
 
-    ngOnDestroy(): void {
-        clearInterval(this.timer);
+  public imageUrl(): string {
+    return this.event.project.imageUrl || this.blockiesService.getImageForAddress(this.event.eventContractAddress);
+  }
+
+  public pad(n) {
+    return (n < 10 ? '0' : '') + Math.floor(parseInt(n));
+  }
+
+  public async showReceiveTokensModalAsync() {
+    const myTokens = this.tokenBalance / this.totalBid * this.myBid;
+    const result = await this.dialogService.showReceiveTokensModalAsync(this.tokenBalance, this.totalBid, this.myBid, myTokens, this.event.tokenTicker);
+    if (result) {
+      await this.allotmentEventService.receiveTokensAsync(this.event.id, this.event.eventContractAddress);
+      this.canRecieveTokens = false;
     }
+  }
 
-    public getPercentShare() {
-        return (this.myBet * 100) / this.totalBet;
+  public getProjectLink(id) {
+    return decodeURIComponent(
+      this.router.createUrlTree([Paths.Project + '/' + id]).toString()
+    );
+  }
+
+  public getAllotmentEventTimeLeft() {
+    const eventDate = new Date(this.event.finishDate);
+    const currentDate = new Date().getTime();
+
+    const eventDateNumber: number = eventDate.getTime();
+    let secondsLeft = (eventDateNumber - currentDate) / 1000;
+
+    const days = this.pad(secondsLeft / 86400);
+    secondsLeft = secondsLeft % 86400;
+
+    const hours = this.pad((secondsLeft / 3600));
+    secondsLeft = secondsLeft % 3600;
+
+    const minutes = this.pad(secondsLeft / 60);
+    const seconds = this.pad(secondsLeft % 60);
+
+    if (new Date() < eventDate) {
+      this.event.timer.days = days;
+      this.event.timer.hours = hours;
+      this.event.timer.minutes = minutes;
+      this.event.timer.seconds = seconds;
+    } else {
+      this.finishEvent.emit(this.event.id);
     }
-
-    public async showParticipateDialogAsync() {
-        if (await this.authenticationService.authenticateAsync()) {
-            const participateResult = await this.dialogService.showParticipateDialog(<AllotmentEventParticipateDialogData>{
-                balance: this.balance,
-                totalBet: this.totalBet,
-                myBet: this.myBet,
-                tokenBalance: this.tokenBalance
-            });
-            if (participateResult) {
-                await this.allotmentEventService.participateAsync(this.event.id, participateResult);
-            }
-        }
-    }
-
-    public imageUrl(): string {
-        return this.event.project.imageUrl || this.blockiesService.getImageForAddress(this.event.eventContractAddress);
-    }
-
-    public pad(n) {
-        return (n < 10 ? '0' : '') + Math.floor(parseInt(n));
-    }
-
-    public async showReceiveTokensModalAsync() {
-        const myTokens = this.tokenBalance / this.totalBet * this.myBet;
-        const result = await this.dialogService.showReceiveTokensModalAsync(this.tokenBalance, this.totalBet, this.myBet, myTokens, this.event.tokenTicker);
-        if (result) {
-            await this.allotmentEventService.receiveTokensAsync(this.event.id, this.event.eventContractAddress);
-            this.canRecieveTokens = false;
-        }
-    }
-
-    public getProjectLink(id) {
-        return decodeURIComponent(
-            this.router.createUrlTree([Paths.Project + '/' + id]).toString()
-        );
-    }
-
-    public getAllotmentEventTimeLeft() {
-        const eventDate = new Date(this.event.finishDate);
-        const currentDate = new Date().getTime();
-
-        const eventDateNumber: number = eventDate.getTime();
-        let secondsLeft = (eventDateNumber - currentDate) / 1000;
-
-        const days = this.pad(secondsLeft / 86400);
-        secondsLeft = secondsLeft % 86400;
-
-        const hours = this.pad((secondsLeft / 3600));
-        secondsLeft = secondsLeft % 3600;
-
-        const minutes = this.pad(secondsLeft / 60);
-        const seconds = this.pad(secondsLeft % 60);
-
-        if (new Date() < eventDate) {
-            this.event.timer.days = days;
-            this.event.timer.hours = hours;
-            this.event.timer.minutes = minutes;
-            this.event.timer.seconds = seconds;
-        } else {
-            this.finishEvent.emit(this.event.id);
-        }
-    }
+  }
 }
---
+++ SmartValley.FrontEnd/src/app/utils/only-numbers.directive.ts
@@ -0,0 +1,40 @@
+import {Directive, ElementRef, HostListener, Input} from '@angular/core';
+
+@Directive({
+    selector: '[appOnlyNumbersByDecimal]'
+})
+export class OnlyNumbersByDecimalDirective {
+    private regex: RegExp = new RegExp(/^-?[0-9]+(\.[0-9]*){0,1}$/g);
+    private decimalRegex: RegExp = new RegExp(/\.(.*)/);
+    private specialKeys: Array<string> = [ 'Backspace', 'Tab', 'End', 'Home', '.' ];
+
+    @Input() decimal: number;
+
+    constructor(private el: ElementRef) {
+    }
+
+    @HostListener('keydown', [ '$event' ])
+
+    onKeyDown(event: KeyboardEvent) {
+      if (!this.decimal) {
+        this.decimal = 1;
+      }
+
+      if (this.specialKeys.indexOf(event.key) !== -1) {
+        return;
+      }
+
+      let current: string = this.el.nativeElement.value;
+      let next: string = current.concat(event.key);
+
+      if (next.match(this.decimalRegex)) {
+        if (next.match(this.decimalRegex)[1].length > this.decimal) {
+          event.preventDefault();
+        }
+      }
+
+      if (next && !String(next).match(this.regex)) {
+          event.preventDefault();
+      }
+    }
+}
