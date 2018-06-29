cfa0abbfdf87dbebe324ed2a26f49ad92968fa2e
Kabanov Viktor <kabanov-viktor92@mail.ru>
2018-06-29 16:12:32 +0300

    SV-1480 add integer checer ti format number

--- SmartValley.FrontEnd/src/app/utils/format-number.pipe.ts
+++ SmartValley.FrontEnd/src/app/utils/format-number.pipe.ts
@@ -7,9 +7,12 @@ import {isNullOrUndefined} from 'util';
 })
 export class FormatNumberPipe implements PipeTransform {
 
-  transform(val: BigNumber | number): string {
+  transform(val: BigNumber | number, showDecimalIfInteger = false ): string {
     if (typeof val === 'number') {
         val = new BigNumber(val);
+    }
+    if (!showDecimalIfInteger && val.decimalPlaces() === 0) {
+      return val.toFormat(0, 0);
     }
     if (!isNullOrUndefined(val)) {
       return val.toFormat(3, 3);
