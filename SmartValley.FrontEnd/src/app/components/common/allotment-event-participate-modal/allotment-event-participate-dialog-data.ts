4524ce5d1425e5da77c0aeacf968924cd151f180
Kabanov Viktor <kabanov-viktor92@mail.ru>
2018-07-02 16:55:56 +0300

    fix build

--- SmartValley.FrontEnd/src/app/components/common/allotment-event-participate-modal/allotment-event-participate-dialog-data.ts
+++ SmartValley.FrontEnd/src/app/components/common/allotment-event-participate-modal/allotment-event-participate-dialog-data.ts
@@ -8,5 +8,5 @@ export interface AllotmentEventParticipateDialogData {
     tokenBalance: BigNumber;
     decimals: number;
     ticker: string;
-    svtDecimals: BigNumber;
+    svtDecimals: number;
 }
