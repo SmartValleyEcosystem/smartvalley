d07d4ac98dcf3e74235d4e0075f1545669f7490c
Konstantin Zhukov <kz@icolab.com>
2018-07-02 15:38:09 +0200

    SV-1468

--- SmartValley.WebApi/Contracts/Responses/ContractResponse.cs
+++ SmartValley.WebApi/Contracts/Responses/ContractResponse.cs
@@ -1,9 +1,20 @@
-﻿namespace SmartValley.WebApi.Contracts.Responses
+﻿using SmartValley.Ethereum.Contracts;
+
+namespace SmartValley.WebApi.Contracts.Responses
 {
     public class ContractResponse
     {
         public string Address { get; set; }
 
         public string Abi { get; set; }
+
+        public static ContractResponse FromOptions(ContractOptions contractOptions)
+        {
+            return new ContractResponse
+                   {
+                       Address = contractOptions.Address,
+                       Abi = contractOptions.Abi
+                   };
+        }
     }
 }
