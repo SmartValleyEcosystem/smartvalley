fafb0aba65ca80ca0fb57b9f8a0b8d7802c0a88d
Kabanov Viktor <kabanov-viktor92@mail.ru>
2018-07-04 13:16:41 +0300

    SV-1520 fix changes

--- SmartValley.FrontEnd/src/environments/environment.ts
+++ SmartValley.FrontEnd/src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
     production: false,
-    baseUrl: 'https://smartvalleymvp-staging.azurewebsites.net/',
+    baseUrl: 'http://localhost:5000',
     network_id: '4',
     etherscan_host: 'https://rinkeby.etherscan.io'
   }
